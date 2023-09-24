37b2cdb92795d492cfc022132c60d4ee05ecd0c4
Michael Turewicz <miantur@web.de>
2023-09-24 16:31:20 +0200

    Add files via upload

---
+++ R/plotPCA.r
@@ -0,0 +1,14 @@
+#' Heat map with hierarchical clustering 
+#'
+#' Plots a PCA for the data given via "input.path" and writes it to a pdf file
+#' in the same location. 
+#' @param input.path Path to a tab-separated txt file (note: path needs to be in
+#' Linux style, i.e. "/" instead of "\"). 
+#' @return Plot with the desired heat map with hierarchical clustering exported
+#' to a pdf file in "input.path". 
+#' @examples 
+#' plotPCA("path/to/data-file.txt");
+#' @export
+plotPCA <- function(input.path=NULL){
+  print(input.path)
+}
